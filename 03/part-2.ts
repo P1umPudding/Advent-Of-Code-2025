05425f5c3700dad0a57792426c2e40833013864e
Kolja <kolja.kempe@web.de>
2025-12-03 13:29:59 +0100

    commet patrt 2

--- 03/part-2.ts
+++ 03/part-2.ts
@@ -7,8 +7,8 @@ const digitAmount = 12;
 let total = 0;
 
 for (const line of lines) {
-    const digits: number[] = new Array(digitAmount).fill(0);
-    const digitPos: number[] = new Array(digitAmount).fill(0);
+    const digits: number[] = new Array(digitAmount).fill(0); // 0 = first digit ...
+    const digitPos: number[] = new Array(digitAmount).fill(0); // shifted by one as we don't need the last digitPos but need a start pos for the first digit (we act like the -1. digit is at position -1 so we start at 0)
     digitPos[0] = -1;
 
     let result = 0;
@@ -16,13 +16,15 @@ for (const line of lines) {
     for (let d = 0; d < digitAmount; d++) {
         for (let i = digitPos[d] + 1; i < line.length - (digitAmount - d - 1); i++) {
             const currentDigit = parseInt(line.charAt(i));
+
             if (currentDigit > digits[d]) {
-                digits[d] = currentDigit;
                 digitPos[d + 1] = i;
+                digits[d] = currentDigit;
             }
-            if (digits[d] == 9) break;
+            if (digits[d] == 9) break; // doesn't get any bigger
         }
-        result += digits[d] * Math.pow(10, digitAmount - d - 1);
+
+        result += digits[d] * Math.pow(10, digitAmount - d - 1); // build up number again (e.g. 9 * 100 + 5 * 10 + 7 = 957)
     }
 
     total += result;
